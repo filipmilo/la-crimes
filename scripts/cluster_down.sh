356d0df16f5633c203a610671137113d7944d0cc
Filip Milosevic <filip.milosevic29@gmail.com>
2025-02-17 21:56:09 +0100

    Add containerized modules for packet processing

---
+++ scripts/cluster_down.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "> Bringing down cluster services"
+
+read -p "> Delete volumes? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo ">> Shutting down Metabase"
+	docker compose -f Metabase/docker-compose.yml down -v
+
+	echo ">> Shutting down Airflow"
+	docker compose -f Airflow/docker-compose.yml down -v
+
+	echo ">> Shutting down Apache Spark"
+	docker compose -f Apache-Spark/docker-compose.yml down -v
+
+	echo ">> Shutting down Hadoop"
+	docker compose -f Hadoop/docker-compose.yml down -v
+else
+	echo ">> Shutting down Metabase"
+	docker compose -f Metabase/docker-compose.yml down
+
+	echo ">> Shutting down Airflow"
+	docker compose -f Airflow/docker-compose.yml down
+
+	echo ">> Shutting down Apache Spark"
+	docker compose -f Apache-Spark/docker-compose.yml down
+
+	echo ">> Shutting down Hadoop"
+	docker compose -f Hadoop/docker-compose.yml down
+
+fi
+
+echo "> Deleting 'asvsp' network"
+docker network rm asvsp
+
---
+++ scripts/cluster_up.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "> Starting up cluster"
+echo "> Creating docker network 'asvsp'"
+docker network create asvsp
+
+ssh_server_startup_cmd='bash -c "/usr/sbin/sshd"'
+
+echo ">> Starting up HDFS"
+docker compose -f Hadoop/docker-compose.yml up -d
+sleep 5
+docker exec -it namenode $ssh_server_startup_cmd
+
+echo ">> Starting up Apache Spark"
+docker compose -f Apache-Spark/docker-compose.yml up -d
+sleep 15
+docker exec -it spark-master $ssh_server_startup_cmd
+
+echo ">> Starting up Airflow"
+docker compose -f Airflow/docker-compose.yml up -d
+sleep 25
+echo ">> Setting up Airflow objects"
+cmd='bash -c "/opt/airflow/config/setupObjects.sh"'
+docker exec -it airflow-airflow-webserver-1 $cmd
+
+echo ">> Starting up Metabase"
+docker compose -f Metabase/docker-compose.yml up -d
+
